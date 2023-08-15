a3689a1bb5c2419041a5e0a3e258d5a2fd79ca3b
rChrisb <5767@holbertonstudents.com>
2023-08-15 16:01:17 +0200

    Lockboxes - a method that determines if all the boxes can be opened

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    open_boxes = [False] * len(boxes)
+    open_boxes[0] = True
+    useful_keys = [0]
+
+    while useful_keys:
+        box_index = useful_keys.pop()
+        for key in boxes[box_index]:
+            if open_boxes[key] is False:
+                open_boxes[key] = True
+                useful_keys.append(key)
+    return all(open_boxes)
