3d1eb198f0ec6ad7f74dd179eee485fd54aded37
rChrisb <5767@holbertonstudents.com>
2023-08-28 13:45:25 +0200

    Heap

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * heap_insert -  inserts a value into a Max Binary Heap
+ * @root:  double pointer to the root node of the Heap
+ * @value: value to store in the node to be inserted
+ * Return: pointer to the inserted node, or NULL on failure
+ */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+heap_t *inserted, *parent;
+
+    if (!root)
+        return (NULL);
+
+    inserted = binary_tree_node(NULL, value);
+    if (!inserted)
+        return (NULL);
+
+    if (!*root)
+        return (*root = inserted);
+
+    parent = *root;
+
+
+    return (inserted);
+}
+
