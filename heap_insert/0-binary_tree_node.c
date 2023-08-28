b37ced8187ac7b1b419fb9847ce5211368218a7f
rChrisb <5767@holbertonstudents.com>
2023-08-28 12:51:36 +0200

    New node

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -14,11 +14,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-	if (node == NULL )
+	if (node == NULL)
 		return (NULL);
 
 	node->parent = parent;
 	node->n = value;
+	node->right = NULL;
+	node->left = NULL;
 
 	return (node);
 }
