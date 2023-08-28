88da8b8d7813c2305ba36b633142bfe99dc1e16c
rChrisb <5767@holbertonstudents.com>
2023-08-28 13:49:52 +0200

    Heap

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -20,10 +20,11 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 
+typedef struct binary_tree_s heap_t;
 
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
+heap_t *heap_insert(heap_t **root, int value);
 void binary_tree_print(const binary_tree_t *tree);
 
 
