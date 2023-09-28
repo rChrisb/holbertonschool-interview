473f9a04bf876468fa21b8c0a67b2dffc72b3e80
rChrisb <5767@holbertonstudents.com>
2023-09-29 01:47:45 +0200

    Log parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -8,34 +8,35 @@ line_count = 0
 
 try:
     for line in sys.stdin:
+        line = line.strip()
 
         parts = line.split()
-        if len(parts) >= 9:
-            ip_address = parts[0]
-            status_code = parts[-2]
+
+        if len(parts) >= 9 and parts[-3] == "HTTP/1.1" and parts[-4] in\
+                status_codes:
+            status_code = parts[-4]
             file_size = parts[-1]
 
-            if ip_address.count('.') == 3 and status_code.isdigit() and \
-                    file_size.isdigit():
-                if status_code in status_codes:
-                    status_count[status_code] += 1
-
+            try:
                 file_size = int(file_size)
                 total_size += file_size
+                status_count[status_code] += 1
+            except ValueError:
+                pass
 
-                line_count += 1
+        line_count += 1
 
-                if line_count % 10 == 0:
-                    print("File size: {}".format(total_size))
-                    for code in sorted(status_codes, key=lambda x: int(x)):
-                        if status_count[code] > 0:
-                            print("{}: {}".format(code, status_count[code]))
+        if line_count % 10 == 0:
+            print("File size: {}".format(total_size))
+            for code in sorted(status_codes):
+                if status_count[code] > 0:
+                    print("{}: {}".format(code, status_count[code]))
 
 except KeyboardInterrupt:
     pass
 
 finally:
     print("File size: {}".format(total_size))
-    for code in sorted(status_codes, key=lambda x: int(x)):
+    for code in sorted(status_codes):
         if status_count[code] > 0:
             print("{}: {}".format(code, status_count[code]))
