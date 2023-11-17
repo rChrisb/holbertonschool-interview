0169c8bbea0b42a6f677fbf7044440a859e93d55
rChrisb <5767@holbertonstudents.com>
2023-11-17 15:36:00 +0100

    chess game

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,48 +1,25 @@
-#!/usr/bin/python3
-
 import sys
 
-def is_safe(board, row, col, n):
+def is_safe(board, row, col):
     # Check if there is a queen in the current row on the left side
     for i in range(col):
-        if board[row][i] == 1:
+        if board[i] == row or board[i] - i == row - col or board[i] + i == row + col:
             return False
-
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
     return True
+
+def print_solution(board):
+    solution = [[i, board[i]] for i in range(len(board))]
+    print(solution)
 
 def solve_nqueens_util(board, col, n):
     if col == n:
-        print_solution(board, n)
+        print_solution(board)
         return
 
     for i in range(n):
-        if is_safe(board, i, col, n):
-            board[i][col] = 1
+        if is_safe(board, i, col):
+            board[col] = i
             solve_nqueens_util(board, col + 1, n)
-            board[i][col] = 0
-
-def print_solution(board, n):
-    solution = []
-    for i in range(n):
-        row = ""
-        for j in range(n):
-            if board[i][j] == 1:
-                row += "Q"
-            else:
-                row += "."
-        solution.append(row)
-    print("\n".join(solution))
-    print()
 
 def nqueens(n):
     if not isinstance(n, int):
@@ -53,7 +30,7 @@ def nqueens(n):
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0 for _ in range(n)] for _ in range(n)]
+    board = [-1] * n
     solve_nqueens_util(board, 0, n)
 
 if __name__ == "__main__":
