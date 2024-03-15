af261e9de2ed9748f1efefb877b9244fb577b54e
rChrisb <5767@holbertonstudents.com>
2024-03-15 19:48:55 +0100

    radix

---
+++ radix_sort/0-radix_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+#include <stdlib.h>
+
+/**
+ * get_max - Returns the maximum element from an array
+ * @array: The array to find the maximum element from
+ * @size: The size of the array
+ *
+ * Return: The maximum element
+ */
+int get_max(int *array, size_t size)
+{
+	int max = array[0];
+	size_t i;
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
+}
+
+/**
+ * count_sort - Performs counting sort based on a specific digit
+ * @array: The array to be sorted
+ * @size: The size of the array
+ * @exp: The exponent to determine the significant digit
+ */
+void count_sort(int *array, size_t size, int exp)
+{
+	int *output = malloc(sizeof(int) * size);
+	int count[10] = {0};
+	size_t i;
+
+	if (output == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
+
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for (i = size - 1; i < (size_t)-1; i--)
+	{
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+
+	free(output);
+}
+
+/**
+ * radix_sort - Sorts an array of integers in ascending order using Radix sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int max, exp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	max = get_max(array, size);
+
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		count_sort(array, size, exp);
+		print_array(array, size);
+	}
+}
