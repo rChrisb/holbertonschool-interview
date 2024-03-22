fb931988b786629bc455c908f742b3c74c4cb8b8
rChrisb <5767@holbertonstudents.com>
2024-03-22 13:30:42 +0100

    island perimeter

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""island peremiter"""
 
 def island_perimeter(grid):
+    """returns perimeter"""
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
