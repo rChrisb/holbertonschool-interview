8e1fca706f40caa1a21ae79322f6db1a24bf1026
rChrisb <5767@holbertonstudents.com>
2024-03-22 13:35:52 +0100

    island perimeter

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -3,7 +3,7 @@
 
 
 def island_perimeter(grid):
-    """returns perimeter"""
+    """returns island perimeter"""
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
@@ -11,10 +11,18 @@ def island_perimeter(grid):
                 # Check all 4 directions for water or boundary
                 if i > 0 and grid[i-1][j] == 0:
                     perimeter += 1
+                else:
+                    perimeter += 1 if i == 0 else 0
                 if i < len(grid) - 1 and grid[i+1][j] == 0:
                     perimeter += 1
+                else:
+                    perimeter += 1 if i == len(grid) - 1 else 0
                 if j > 0 and grid[i][j-1] == 0:
                     perimeter += 1
+                else:
+                    perimeter += 1 if j == 0 else 0
                 if j < len(grid[0]) - 1 and grid[i][j+1] == 0:
                     perimeter += 1
+                else:
+                    perimeter += 1 if j == len(grid[0]) - 1 else 0
     return perimeter
