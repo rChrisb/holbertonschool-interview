e6bd7e54b3924546c6a42854bbd44ca2d80cc6e8
rChrisb <5767@holbertonstudents.com>
2024-04-04 21:20:57 +0200

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -4,7 +4,7 @@
 
 /**
  * wildcmp - Compares two strings and returns 1 if they can be considered
- *		   identical, otherwise returns 0.
+ * identical, otherwise returns 0.
  * @s1: First string.
  * @s2: Second string, can contain the special character '*'.
  *
@@ -12,23 +12,25 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-        return 1;
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (!s2)
+		return (0);
 
-    if (*s2 == '*')
-    {
-        if (wildcmp(s1, s2 + 1))
-            return 1;
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1))
+			return (1);
 
-        if (*s1 != '\0' && wildcmp(s1 + 1, s2))
-            return 1;
-    }
+		if (*s1 != '\0')
+			return (wildcmp(s1 + 1, s2));
+	}
 
-    if (*s1 == *s2)
-        return wildcmp(s1 + 1, s2 + 1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-    if (*s2 != '*')
-        return 0;
+	if (*s2 != '*')
+		return (0);
 
-    return wildcmp(s1, s2 + 1);
+	return (wildcmp(s1, s2 + 1));
 }
